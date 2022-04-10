7d130f4710eb03a45b63da3c4941dc0b693a1efc
cucu <cuneytdonmez8@gmail.com>
2022-04-10 10:14:09 +0300

    Not found Page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Post from "./postList/Post";
 import AddPost from "./addPost/AddPost";
 import AddUser from "./addUser/AddUser";
+import NotFound from "./notFound/NotFound";
 
-export {  Post, AddPost, AddUser };
+export {  Post, AddPost, AddUser, NotFound, };
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { AddPost, Post, AddUser } from '../pages';
+import { AddPost, Post, AddUser, NotFound } from '../pages';
 
 const AppRouter = () => (
     <Routes>
         <Route path="/" element={< Post />} />
         <Route path="/add-post" element={<AddPost />} />
         <Route path="/add-user" element={<AddUser />} />
+        <Route path='*' element={<NotFound />} />
     </Routes>
 );
 
